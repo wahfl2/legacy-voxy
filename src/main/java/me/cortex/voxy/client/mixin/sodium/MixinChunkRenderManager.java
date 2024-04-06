c0e38c110c5a69bbda04a30da15b81c90740f6fd
wahfl2 <59855656+wahfl2@users.noreply.github.com>
2024-04-06 01:58:57 -0500

    Combine duplicate mixin classes

--- src/main/java/me/cortex/voxy/client/mixin/sodium/MixinChunkRenderManager.java
+++ src/main/java/me/cortex/voxy/client/mixin/sodium/MixinChunkRenderManager.java
@@ -1,17 +1,31 @@
 package me.cortex.voxy.client.mixin.sodium;
 
 import com.gtnewhorizons.angelica.compat.toremove.MatrixStack;
+import me.cortex.voxy.client.config.VoxyConfig;
 import me.cortex.voxy.client.core.IGetVoxelCore;
 import me.jellysquid.mods.sodium.client.render.chunk.ChunkRenderManager;
 import me.jellysquid.mods.sodium.client.render.chunk.passes.BlockRenderPass;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.WorldClient;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = ChunkRenderManager.class, remap = false)
 public class MixinChunkRenderManager {
+
+    @Shadow @Final private WorldClient world;
+
+    @Inject(method="unloadChunk", at=@At("TAIL"))
+    private void injectIngest(int x, int z, CallbackInfo ci) {
+        var core = ((IGetVoxelCore)Minecraft.getMinecraft().renderGlobal).getVoxelCore();
+        if (core != null && VoxyConfig.ingestEnabled) {
+            core.enqueueIngest(world.getChunkProvider().provideChunk(x, z));
+        }
+    }
 
     @Inject(method = "renderLayer", at = @At(value = "INVOKE", target = "Lme/jellysquid/mods/sodium/client/render/chunk/ChunkRenderBackend;end(Lcom/gtnewhorizons/angelica/compat/toremove/MatrixStack;)V", shift = At.Shift.BEFORE))
     private void injectRender(MatrixStack matrixStack, BlockRenderPass pass, double x, double y, double z, CallbackInfo ci) {
